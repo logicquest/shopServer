aa1da879cc633c88f845902dd9fc1f3e4cb4174c
Chandrasekar Hariharan - Vendor <vn0aly0@m-C02SL0KJG8WM.local>
2017-08-01 17:02:09 -0700

    sc

--- api/controllers/CheckpostcodeController.js
+++ api/controllers/CheckpostcodeController.js
@@ -16,20 +16,22 @@ module.exports = {
             console.log("In the app");
             console.log(req.body);
             console.log(requestbody.result.parameters.postcode[0]);
+
+            res.send('Hurray it worked!!');
             // Parse the Postcode
-            var postcode= requestbody.result.parameters.postcode[0];
-            var translateUrl='https://groceries.asda.com/api/user/checkpostcode?listcnc=true&postcode='+postcode;
-            fetch(translateUrl)
-                .then(response => response.json())
-                .then(data => {
-                console.log(data);
-                console.log(JSON.stringify(data));
-                res.ok("Hurray! We Deliver to the"+ postcode+" postcode");
-                })
-                .catch(err => {
-                console.log(err);
-                res.json(err);
-                });
+            // var postcode= requestbody.result.parameters.postcode[0];
+            // var translateUrl='https://groceries.asda.com/api/user/checkpostcode?listcnc=true&postcode='+postcode;
+            // fetch(translateUrl)
+            //     .then(response => response.json())
+            //     .then(data => {
+            //     console.log(data);
+            //     console.log(JSON.stringify(data));
+            //     res.ok("Hurray! We Deliver to the"+ postcode+" postcode");
+            //     })
+            //     .catch(err => {
+            //     console.log(err);
+            //     res.json(err);
+            //     });
         }
         
 };
